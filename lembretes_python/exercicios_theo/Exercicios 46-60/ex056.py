51056457a921212da4e14cc4fb4b6548a110ba39
OkagawaTheo <63313909+OkagawaTheo@users.noreply.github.com>
2020-06-16 14:11:21 -0300

    exercicios

--- lembretes_python/exercicios_theo/Exercicios 46-60/ex056.py
+++ lembretes_python/exercicios_theo/Exercicios 46-60/ex056.py
@@ -2,6 +2,9 @@
 #   media de idade
 #   homem mais velho
 #   quantas mulheres tem menos de 20 anos
+#   joga as idades para a lista com append e dps veja a maior idade
+
+
 from time import sleep
 
 media = 0
@@ -19,10 +22,7 @@ for pessoas in range(1, 5):
     media += idade
 media = media / 4  #    transforma 'media' em uma média de verdade  
 
-mais_velho = []
+mais_velho = [] # lista vazia pra ver qual a maior idade 
 
 
-print(f'{media:.2f}')
-
-    if sexo == 'm': #   joga as idades para a lista com append e dps ver a maior
-        
+print(f'A média de idade do grupo é de {media:.2f}')
