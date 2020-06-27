e5cdd5652242b3c87b61ddbec7442c5c74058c7c
Miguel-Perfume <miguelpafume@hotmail.com>
2020-06-27 18:27:48 -0300

    Arrumado ex 88

--- exercícios_python/exercíciosMiguel/Curso_Em_Vídeo/Exercícios_76-90/Mundo_3_Ex_88.py
+++ exercícios_python/exercíciosMiguel/Curso_Em_Vídeo/Exercícios_76-90/Mundo_3_Ex_88.py
@@ -9,9 +9,12 @@ quantidade = int(input("Quantos jogos deseja gerar?\n> "))
 print("-+" * 15)
 
 for i in range(0, quantidade):
-    for k in range(0, 6):
-        numeros.append(randint(1, 60))
+    while len(numeros) < 6:
+        temp = randint(1, 60)
+        if not temp in numeros:
+            numeros.append(temp)
 
+    numeros.sort()
     jogos.append(numeros[:])
     numeros.clear()
 
