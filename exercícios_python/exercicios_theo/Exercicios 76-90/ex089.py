37032bd2d697267024daef2fbd84550684756e70
OkagawaTheo <63313909+OkagawaTheo@users.noreply.github.com>
2020-07-09 18:10:56 -0300

    nao sei

--- exercícios_python/exercicios_theo/Exercicios 76-90/ex089.py
+++ exercícios_python/exercicios_theo/Exercicios 76-90/ex089.py
@@ -4,21 +4,20 @@ fichaAlunos = list()
 
 
 while True:
-    nome = str(input('\nNome do aluno: ')) # nome da pessoa
-    nota1 = float(input('Nota 1: ')) # 1° nota
-    nota2 = float(input('Nota 2: ')) # 2° nota
-    media = (nota1 + nota2) / 2 # calcula a média de cada aluno
+    nome = str(input('\nNome do aluno: '))  # nome da pessoa
+    nota1 = float(input('Nota 1: '))  # 1° nota
+    nota2 = float(input('Nota 2: '))  # 2° nota
+    media = (nota1 + nota2) / 2  # calcula a média de cada aluno
 
+    fichaAlunos.append([nome, [nota1, nota2], media])  # adiciona pra lista
 
-    fichaAlunos.append([nome, [nota1, nota2], media]) # adiciona pra lista
-    
     resposta = str(input('Deseja continuar? [S/N] '))
-    if resposta in 'Nn': # se a resposta for NAO
+    if resposta in 'Nn':  # se a resposta for NAO
         break
- 
+
 print('='*60)
 
-# variavel => i = numero do aluno 
+# variavel => i = numero do aluno
 
 print(f'{"No.":<4} {"Nome":<10} {"Média":<8}')
 print()
